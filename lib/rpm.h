20230728b1fe096973be330845833312fc422b48
Wer-Wolf <robo-armin@gmx.de>
2020-03-22 19:48:15 +0100

    adding power management options

--- lib/rpm.h
+++ lib/rpm.h
@@ -3,6 +3,8 @@
 #include <avr/power.h>
 #include <util/atomic.h>
 
+#define SENSOR_ENABLE_PIN PA0
+
 #define capture_active() (TIMSK1 & (1 << ICIE1))
 
 static volatile uint16_t capture_value = 0; //Atomar abfragen!
@@ -19,12 +21,31 @@ uint16_t get_capture_value() {
 inline void rpm_init() {
     //Komparator initialisieren (Interrupt um Timer zu starten)
     DIDR0 |= (1 << ADC2D) | (1 << ADC1D);
-    ACSR |= (1 << ACIC) | (1 << ACIE) | (1 << ACIS0) | (1 << ACIS1);
+    ACSR |= (1 << ACD) | (1 << ACIC) | (1 << ACIS0) | (1 << ACIS1);
+    power_adc_disable();
     //16-Bit Timer initialisieren (Presacler 64), Wert -5
     TCCR1B |= (1 << ICNC1);
     //Overflow (und Capture)
     TIMSK1 |= (1 << TOIE1);
     power_timer1_disable();
+}
+
+void rpm_enable() {
+    PORTA |= (1 << PA0); //DDrx anpassen!
+    ACSR &= ~(1 << ACD);
+    ACSR |= (1 << ACIE);
+    power_timer1_enable();
+    power_adc_enable();
+}
+
+void rpm_disable() {
+    ACSR &= ~(1 << ACIE);
+    TCCR1B &= ~((1 << CS11) | (1 << CS10)); //Timer stoppen
+    TIMSK1 &= ~(1 << ICIE1); //Interrupt deaktivieren
+    power_timer1_disable();
+    ACSR |= (1 << ACD);
+    power_adc_disable();
+    PORTA &= ~(1 << PA0);
 }
 
 ISR(ANA_COMP_vect) {
