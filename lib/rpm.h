a4aad45455b3c5d20acc1b1b3b0e4786a4b0c2a8
Wer-Wolf <robo-armin@gmx.de>
2020-06-05 23:53:49 +0200

    Improving controls

--- lib/rpm.h
+++ lib/rpm.h
@@ -7,6 +7,15 @@
 
 #define capture_active() (TIMSK1 & (1 << ICIE1))
 
+/* Rising Edge (= Auschnitt beginnt) */
+#define ACSR_DEFAULT ((1 << ACD) | (1 << ACIC) | (1 << ACIS1) | (1 << ACIS0))
+
+/* Falling Edge (= Ausschnitt endet), Prescaler 64, Wert - 5 */
+#define TCCR1B_DEFAULT ((1 << ICNC1) | (1 << CS11) | (1 << CS10))
+
+/* Overflow und Capture Interrupt */
+#define TIMSK1_DEFAULT ((1 << ICIE1) | (1 << TOIE1))
+
 static volatile uint16_t capture_value = 0; //Atomar abfragen!
 //Bleibt bei Overflow auf 0xFFFF stehen
 
@@ -18,55 +27,53 @@ uint16_t get_capture_value() {
     return value;
 }
 
+void start_capture() {
+    PORTA |= (1 << PA0); //DDrx anpassen!
+    power_adc_enable();
+    ACSR = ACSR_DEFAULT & ~(1 << ACD);
+    ACSR = ACSR_DEFAULT & ~(1 << ACD) & (1 << ACIE);
+}
+
+void stop_capture() {
+    ACSR = ACSR_DEFAULT;
+    ACSR = ACSR_DEFAULT & (1 << ACI); //Flag löschen
+    power_adc_disable();
+    TIMSK1 = TIMSK1_DEFAULT & ~(1 << ICIE1); //Interrupt deaktivieren
+    /* Counter stoppen */
+    TCCR1B = TCCR1B_DEFAULT & ~(1 << CS11) & ~(1 << CS10);
+    TIFR1 = (1 << TOV1) | (1 << ICF1); //Flags löschen
+    power_timer1_disable();
+    PORTA &= ~(1 << PA0);
+}
+
 inline void rpm_init() {
     //Komparator initialisieren (Interrupt um Timer zu starten)
     DIDR0 |= (1 << ADC2D) | (1 << ADC1D);
-    ACSR |= (1 << ACD) | (1 << ACIC) | (1 << ACIS0) | (1 << ACIS1);
+    ACSR = ACSR_DEFAULT;
     power_adc_disable();
-    //16-Bit Timer initialisieren (Presacler 64), Wert -5
-    TCCR1B |= (1 << ICNC1);
-    //Overflow (und Capture)
-    TIMSK1 |= (1 << TOIE1);
+    /* Counter stoppen */
+    TCCR1B = TCCR1B_DEFAULT & ~(1 << CS11) & ~(1 << CS10);
+    //Erst nur Overflow
+    TIMSK1 = TIMSK1_DEFAULT & ~(1 << ICIE1);
     power_timer1_disable();
-}
-
-void rpm_enable() {
-    PORTA |= (1 << PA0); //DDrx anpassen!
-    power_adc_enable();
-    ACSR &= ~(1 << ACD);
-    ACSR |= (1 << ACIE);
-}
-
-void rpm_disable() {
-    ACSR &= ~(1 << ACIE);
-    TIMSK1 &= ~(1 << ICIE1); //Interrupt deaktivieren
-    TCCR1B &= ~((1 << CS11) | (1 << CS10)); //Timer stoppen
-    power_timer1_disable();
-    ACSR |= (1 << ACD);
-    power_adc_disable();
-    PORTA &= ~(1 << PA0);
 }
 
 ISR(ANA_COMP_vect) {
     if(!capture_active()) { //Prellungen kompensieren
         power_timer1_enable();
         TCNT1 = 0x0000;
-        TIFR1 |= (1 << ICF1);
-        TIMSK1 |= (1 << ICIE1); //Interrupt aktivieren
-        TCCR1B |= (1 << CS11) | (1 << CS10); //Timer starten
+        TIFR1 = (1 << ICF1);
+        TIMSK1 = TIMSK1_DEFAULT; //Interrupt aktivieren
+        TCCR1B = TCCR1B_DEFAULT; //Timer starten
     }
 }
 
 ISR(TIM1_CAPT_vect) {
     capture_value = ICR1;
-    TCCR1B &= ~((1 << CS11) | (1 << CS10)); //Timer stoppen
-    TIFR1 |= (1 << TOV1); //Oveflow-Flag löschen
-    power_timer1_disable();
+    stop_capture();
 }
 
 ISR(TIM1_OVF_vect) {
-    TIMSK1 &= ~(1 << ICIE1); //Interrupt deaktivieren
-    TCCR1B &= ~((1 << CS11) | (1 << CS10)); //Timer stoppen
-    capture_value = 0xFFFF;
-    power_timer1_disable();
+    capture_value = 0xFFFF; //Overflow
+    stop_capture();
 }
