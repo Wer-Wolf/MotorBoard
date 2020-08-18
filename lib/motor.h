1cf6101e15b36079e793e713a008abc57a1eb4ff
Wer-Wolf <robo-armin@gmx.de>
2020-08-18 14:16:32 +0200

    Using the correct registers

--- lib/motor.h
+++ lib/motor.h
@@ -5,10 +5,15 @@
 #define L293D_3A PB0 //DDRB
 #define L293D_4A PB1
 
-#define MOTOR_A_SET_LEFT DDRA |= (1 << L293D_1A); DDRA &= ~(1 << L293D_2A)
-#define MOTOR_A_SET_RIGHT DDRA |= (1 << L293D_2A); DDRA &= ~(1 << L293D_1A)
-#define MOTOR_A_STOP DDRA &= ~((1 << L293D_1A)| (1 << L293D_2A))
+/*#define MOTOR_A_STATUS (DDRA & ((1 << L293D_1A) | (1 << L293D_2A)))
+#define MOTOR_A_STOP ((1 << L293D_1A) | (1 << L293D_2A))
+#define MOTOR_A_LEFT (1 << L293D_1A)
+#define MOTOR_A_RIGHT (1 << L293D_2A)*/
 
-#define MOTOR_B_SET_LEFT DDRB |= (1 << L293D_3A); DDRB &= ~(1 << L293D_4A)
-#define MOTOR_B_SET_RIGHT DDRB |= (1 << L293D_4A); DDRB &= ~(1 << L293D_3A)
-#define MOTOR_B_STOP DDRB &= ~((1 << L293D_3A)| (1 << L293D_4A))
+#define MOTOR_A_SET_LEFT PORTA |= (1 << L293D_1A); PORTA &= ~(1 << L293D_2A)
+#define MOTOR_A_SET_RIGHT PORTA |= (1 << L293D_2A); PORTA &= ~(1 << L293D_1A)
+#define MOTOR_A_SET_STOP PORTA &= ~((1 << L293D_1A) | (1 << L293D_2A))
+
+#define MOTOR_B_SET_LEFT PORTB |= (1 << L293D_3A); PORTB &= ~(1 << L293D_4A)
+#define MOTOR_B_SET_RIGHT PORTB |= (1 << L293D_4A); PORTB &= ~(1 << L293D_3A)
+#define MOTOR_B_SET_STOP PORTB &= ~((1 << L293D_3A) | (1 << L293D_4A))
